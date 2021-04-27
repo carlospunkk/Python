463d4adc02814c689fbf156862f9b72037903dfa
carlospunkk <cromulo.romulo@gmail.com>
2021-04-27 14:10:43 -0300

    adicionado aula modulos e modificaçoes na pas aulaoperadores

---
+++ aula_operadoresA.py
@@ -0,0 +1,106 @@
+#n1 = int(input('um valor'))
+#n2 = int(input('outro valor'))
+#s = n1+n2
+#d = n1/n2
+#m = n1*n2
+#ni = n1 // n2
+#e = n1 ** n2
+#print ('a soma vale {} , a divisão é {:.3f} , a multiplicação é {} , e a divisão inteira é {} , e a exponenciação é {}    '.format (s, d, m, ni, e))
+
+#sucessor e antecessor
+#n1 =int(input('valor'))
+#num=n1
+#a=n1-1
+#s=n1+1
+#print('o sucessor é {} , o antecessor é {} , do numero {} '.format( s, a, num))
+
+
+#Raiz quadra/dobro/triplo
+#n1 = int (input('valor'))
+#dob= n1 * 2
+#tri= n1 * 3
+#ra= n1**(1/2)
+#print('o dobro é {} , o triplo é {} , a raiz quadrada é {} '.format(dob, tri, ra))
+
+#media
+#nota1 = float (input('digite sua nota'))
+#nota2 = float (input('digite nota 2 '))
+#media = (nota1+nota2)/2
+#print('a sua nota final é {} '.format(media))
+
+#conversão metros
+#medida = float (input('digita sua medida em metros'))
+#cm = medida * 100
+#mm = medida * 1000
+#print('a medida em centimetros é {:.0f} cm, a medida em milimetros , {:.0f} mm'.format(cm,mm))
+
+
+
+#metros
+#valor = int (input("digite a metragem"))
+#cen = valor / 100
+#mili = valor/1000
+#print('o valor de centimetros {} , o valor de milimetros é {}'.format(cen , mili))
+
+#tabuada
+#num=int(input('digite numero'))
+#print('-'*10)
+#print('{} X {} = {}'.format( num , 1 , num * 1 ))
+#print('{} X {} = {}'.format( num , 2 , num * 2 ))
+#print('{} X {} = {}'.format( num , 3 , num * 3 ))
+#print('{} X {} = {}'.format( num , 4 , num * 4 ))
+#print('{} X {} = {}'.format( num , 5 , num * 5 ))
+#print('{} X {} = {}'.format( num , 6 , num * 6 ))
+#print('{} X {} = {}'.format( num , 7 , num * 7 ))
+#print('{} X {} = {}'.format( num , 8 , num * 8 ))
+#print('{} X {} = {}'.format( num , 9 , num * 9 ))
+#print('{} X {} = {}'.format( num , 10 , num * 10 ))
+
+
+
+#dolar
+#di = float(input('digite quanto vc tem R$'))
+#dolar = di / 3.27
+#print ('voce  tem {} R$ reais ,e consegue comprar , {:.2f} US$ dolares '.format(di ,dolar))
+
+#areatinta
+#l = float(input('digite largura'))
+#alt = float(input('digite altura'))
+#area= l * alt
+#lit = area/2
+#print('a quantidade de litros para pintar a parede é {} litros '.format(lit))
+
+#preço
+#v = float(input('digite o preço R$'))
+#desc = ( v * 5)/100
+#total = v - desc
+#print('o valor com desconto  de 5%  è , {}  '.format(total))
+
+#funcionario
+sal = float (input('digite seu salario'))
+calc = (sal*15)/100
+novo = sal + calc
+print('seu novo salario com 15% de aumento  é , {} '.format(novo))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ utlizando_modulos.py
@@ -0,0 +1,8 @@
+
+#from math import sqrt, floor
+#num = int( input('digite um numero ') )
+#raiz= sqrt(num)
+#print("a raiz do numero  {} , é igual = {}".format(num  , floor(raiz)))
+import random
+num = random.randint( 1 ,60)
+print(num)
