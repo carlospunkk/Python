498e588bdd08327f61eeb9febb0967fef97ceea3
carlospunkk <cromulo.romulo@gmail.com>
2021-04-29 15:22:06 -0300

    arquivos adicionados caracteres Math

--- utlizando_modulos.py
+++ utlizando_modulos.py
@@ -3,6 +3,110 @@
 #num = int( input('digite um numero ') )
 #raiz= sqrt(num)
 #print("a raiz do numero  {} , é igual = {}".format(num  , floor(raiz)))
-import random
-num = random.randint( 1 ,60)
-print(num)
+
+#import random
+#num = random.randint( 1 ,60)
+#print(num)
+
+#desafio 16
+#import random
+#import random
+#from math import ceil , floor
+#num = float(input('digite um numero real'))
+#print('o numero arredondado é {}'.format(ceil(num)))# arredondei utilizando para cima
+
+#num = float(input('digite um numero real'))
+#print('o numero arredondado é {}'.format(floor(num)))#arredondei pra baixo
+
+#desafio 17 hipotenusa e catetos
+#co = float(input("digite valor cateto oposto"))
+#ca = float(input(" digite cateto adjacente"))
+#hi = (co ** 2 + ca ** 2)**(1/2)
+#print("a hipotenusa vai medir {}".format(hi))
+
+
+
+#desafio 18
+
+
+#desafio 19
+#import math
+#n1= (input("digite o primeiro aluno é :"))
+#n2= (input("segundo aluno é :"))
+#n3= (input("o terceiro aluno :"))
+#n4= (input("o quarto aluno é :"))
+#lista=[n1,n2,n3,n4]
+#alunos = random.choice(lista)
+#print('o aluno sorteado é {} ,'.format(alunos))
+
+#desafio 20
+#n1= (input("digite o primeiro aluno é :"))
+#n2= (input("segundo aluno é :"))
+#n3= (input("o terceiro aluno :"))
+#n4= (input("o quarto aluno é :"))
+#lista = [n1, n2 , n3 ,n4 ]
+#random.shuffle(lista)
+#print(lista)
+
+#...........AULA19..........
+#   cadeia de caracteres
+#frase = ('Curso em Video Python')
+#print('*'.join(frase.split()))
+
+#desafio o nopme com todas letras maiusculas
+#todas minusculas
+#quantas letras tem ao todo sem considerar aspas
+#quantas letras tem o primeiro nome
+
+#nome = str(input('digite seu nome')).strip()
+#print("seu nome maiusculo é :" , nome.upper())
+#print("seu nome maiusculo é :" , nome.lower())
+#print (nome.find(''))
+
+#desafio23
+#numeros = str(input("digite seu numero"))
+#print("centena :", numeros [3])
+#print("dezena :", numeros [2])
+#print("centena :", numeros [1])
+#print("milhar:", numeros [0])
+#print(numeros[0:])
+
+#desafionuemrico26
+#numeros = int(input("digite seu numero"))
+#u = numeros//1 % 10
+#d = numeros//10 % 10
+#c = numeros//100 % 10
+#m= numeros//1000%10
+#print("centena :", u )
+#print("dezena :", d )
+#print("centena :", c )
+#print("milhar:", m )
+
+
+
+
+#desafio24
+#cidade = str(input("digite o  nome da sua cidade"))
+#print(cidade)
+#desafio26
+
+
+#desafio26
+#frase = input("digite uma frase")
+#print (frase.count('a'))
+
+#desafio27
+'''nome = input("digite seu nome inteiro ")
+primeiro = (nome.split())
+print(primeiro[0])
+ultimo = (nome.split())
+print(ultimo[3])'''
+
+
+
+
+
+
+
+
+
