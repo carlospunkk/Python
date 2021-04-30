c6d72cd32334f060accdcd8cee849a348c2bb20a
carlospunkk <cromulo.romulo@gmail.com>
2021-04-29 22:00:18 -0300

    aula condicoes

---
+++ condicoes.py
@@ -0,0 +1,43 @@
+'''nome = str(input('qual seu nome'))
+if nome == ('Gustavo'):
+    print('bonito nome bem vindo !!! ')
+print('bem vindo {} , '.format(nome))'''
+import random
+
+'''n1 = float(input('digite sua nota'))
+n2 = float(input('digite segunda nota '))
+m = (n1+n2)/2
+if m >= 6:
+    print(' sua nota foi {:.1f} muito boa nota '.format(m))
+else:
+    print(' voce foi reprovado estude novamente sua nota {} foi péssima  '.format(m))'''
+#desafio28
+#import math
+#digite = pense
+#print(pense)
+'''pense = random.randint(0 , 5)
+tente = [0,1,2,3,4,5]
+tente = int(input("tente acertar digite um numero de 1 até 5 :  "))
+if tente == pense:
+    print("voce acertou")
+    print("o numero sorteado foi {}".format(pense))
+else:
+    print("voce errou")
+    print("o numero sorteado foi {}".format(pense))'''
+import math
+
+'''velocidade = float(input("digite sua velocidade "))
+multa = (velocidade-80) * 7
+if velocidade >= 80:
+    print("voce foi multado em R${} reais".format(multa))
+else:
+    print("pode seguir em frente")
+print("dirija com segurança !!!")'''
+
+
+
+
+
+
+
+
